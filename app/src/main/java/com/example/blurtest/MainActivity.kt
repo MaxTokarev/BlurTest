a4df267b4fdb76b18e7064cad477ab8127ed3ce6
MaxTokare <maksim_tokarev_111@mail.ru>
2024-01-24 20:43:09 +0400

    fix build

--- app/src/main/java/com/example/blurtest/MainActivity.kt
+++ app/src/main/java/com/example/blurtest/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.example.blurtest
 
-import BlurDialog
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
-import androidx.compose.ui.graphics.BlurEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.blurtest.ui.theme.BlurTestTheme
 import com.skydoves.cloudy.Cloudy
+import xBlur
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +31,21 @@ class MainActivity : ComponentActivity() {
                         .background(Color.Gray)
                         .fillMaxSize(),
                 ) {
-                    com.
-                    Cloudy {
-                        Text(text = "abc")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Cloudy {
+                            Text(text = "Blured text", style = MaterialTheme.typography.bodyLarge)
+                        }
+                        Text(
+                            text = "Custom blur",
+                            modifier = Modifier.xBlur(16, context = LocalContext.current)
+                        )
+                        Box(modifier = Modifier.blur(10.dp)) {
+                            Text(text = "Default Blur")
+                        }
+                        Text(text = "Non Blured text")
                     }
                 }
             }
